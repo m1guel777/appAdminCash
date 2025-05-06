c9f27efb1ef8e633f20a4fb33f89fe21261c8aa2
unknown <granadosmiguel832@gmail.com>
2025-05-05 23:59:04 -0600

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { EmpresasComponent } from './pages/empresas/empresas.component';
+import { PropiedadesComponent } from './pages/propiedades/propiedades.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'empresas', component: EmpresasComponent },
+  { path: 'propiedades', component: PropiedadesComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { EmpresasComponent } from './pages/empresas/empresas.component';
+import { PropiedadesComponent } from './pages/propiedades/propiedades.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HomeComponent,
+    EmpresasComponent,
+    PropiedadesComponent
   ],
   imports: [
     BrowserModule,
