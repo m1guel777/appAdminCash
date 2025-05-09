16611b888ce89d3df129a6efa713313cc8331bec
miguel-eni <miguel.granados@eninetworks.com>
2025-05-08 17:23:24 -0700

    estructura

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy } from '@angular/core';
 
 declare var $: any; // Para poder usar jQuery
 
@@ -8,18 +8,28 @@ declare var $: any; // Para poder usar jQuery
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent implements AfterViewInit{
+export class SidebarComponent implements AfterViewInit , OnDestroy{
+
+  private scriptElement: HTMLScriptElement | undefined;
+
   ngAfterViewInit(): void {
-    $('.right-side-toggle').off('click').on('click', function () {
-      const $sidebar = $('.right-sidebar');
-      if ($sidebar.hasClass('shw-rside')) {
-        $sidebar.removeClass('shw-rside').css('right', '-300px');
-      } else {
-        $sidebar.addClass('shw-rside').css('right', '0');
-      }
-    });
+    this.loadScript('assets/js/custom.min.js');
   }
 
+
+  loadScript(src: string): void {
+    this.scriptElement = document.createElement('script');
+    this.scriptElement.src = src;
+    this.scriptElement.type = 'text/javascript';
+    this.scriptElement.async = false;
+    document.body.appendChild(this.scriptElement);
+  }
+
+  ngOnDestroy(): void {
+    if (this.scriptElement) {
+      document.body.removeChild(this.scriptElement);
+    }
+  }
 
 
 
